7cc2db3606b154814491ec7d729e90c609e7a594
ワタナベ ヒカル <hikalin@raiti-no-MacBook-Air.local>
2012-07-08 16:08:09 +0900

    Initial Commit

---
+++ CutCutCut/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CutCutCut
+//
+//  Created by 光 渡邊 on 12/07/08.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
