43122d04949ab0612f0dc601070c4a9ac393de9f
ワタナベ ヒカル <hikalin@raiti-no-MacBook-Air.local>
2012-07-22 01:16:10 +0900

    パーフェクトナベ導入前

---
+++ CutCutCut/DekiagaLayer.h
@@ -0,0 +1,16 @@
+//
+//  DekiagaLayer.h
+//  Robocurry
+//
+//  Created by 光 渡邊 on 12/07/22.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface DekiagaLayer : CCLayer {
+    
+}
+
+@end
---
+++ CutCutCut/DekiagaLayer.mm
@@ -0,0 +1,14 @@
+//
+//  DekiagaLayer.m
+//  Robocurry
+//
+//  Created by 光 渡邊 on 12/07/22.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "DekiagaLayer.h"
+
+
+@implementation DekiagaLayer
+
+@end
